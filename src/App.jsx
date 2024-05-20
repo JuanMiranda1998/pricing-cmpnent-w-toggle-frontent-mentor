941ceae934a546e1cea6320b75c8f6f9c40e035d
JuanMiranda1998 <mirandajuan1998@outlook.com>
2024-05-20 15:53:06 -0300

    added hover states

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <h1 className="text-3xl">Our Pricing</h1>
         <div className='flex flex-row items-center gap-2 mt-8 mb-10'>
           <p className="text-sm text-[#a9a9b5]">Anually</p>
-          <label className="toggle-switch md:mx-3">
+          <label className="toggle-switch md:mx-3 md:my-0">
             <input type="checkbox" onChange={checkHandler} />
             <div className="toggle-switch-background">
               <div className="toggle-switch-handle"></div>
@@ -29,7 +29,7 @@ function App() {
             <p className='w-full text-center border-t border-b border-[#a9a9b5] mt-10 py-4'>500 GB Storage</p>
             <p className='w-full text-center py-4 border-b border-[#a9a9b5]'>2 Users Allowed</p>
             <p className='w-full text-center py-4 border-b border-[#a9a9b5]'>Send up to 3 GB</p>
-            <button className='w-full py-3 px-4 mt-10 text-sm text-white rounded-md uppercase tracking-widest bg-gradient-to-r from-gradient-1 to-gradient-2'>Learn More</button>
+            <button className='w-full py-3 px-4 mt-10 text-sm text-white rounded-md uppercase tracking-widest bg-gradient-to-r from-gradient-1 to-gradient-2 hover:bg-none hover:text-gradient-2 border border-transparent hover:border-gradient-2 transition-colors ease-in duration-150'>Learn More</button>
           </div>
           <div className='w-[90%] flex flex-col items-center my-4 p-8 md:py-14 md:px-8 bg-gradient-to-b from-gradient-1 to-gradient-2 text-white shadow-lg rounded-md'>
             <h2 className='text-lg'>Professional</h2>
@@ -37,7 +37,7 @@ function App() {
             <p className='w-full text-center border-t border-b border-very-light-grayish-blue mt-10 py-4'>1 TB Storage</p>
             <p className='w-full text-center py-4 border-b border-very-light-grayish-blue'>5 Users Allowed</p>
             <p className='w-full text-center py-4 border-b border-very-light-grayish-blue'>Send up to 10 GB</p>
-            <button className='w-full py-3 px-4 mt-10 text-sm bg-white text-gradient-2 rounded-md uppercase tracking-widest '>Learn More</button>
+            <button className='w-full py-3 px-4 mt-10 text-sm bg-white text-gradient-2 rounded-md uppercase tracking-widest hover:bg-transparent border border-transparent hover:border-white hover:text-white transition-colors ease-in duration-150'>Learn More</button>
           </div>
           <div className='w-[90%] flex flex-col items-center my-4 p-8 bg-white shadow-lg rounded-md md:rounded-none md:rounded-r-md'>
             <h2 className='text-lg'>Master</h2>
@@ -45,7 +45,7 @@ function App() {
             <p className='w-full text-center border-t border-b border-[#a9a9b5] mt-10 py-4'>2 TB Storage</p>
             <p className='w-full text-center py-4 border-b border-[#a9a9b5]'>10 Users Allowed</p>
             <p className='w-full text-center py-4 border-b border-[#a9a9b5]'>Send up to 20 GB</p>
-            <button className='w-full py-3 px-4 mt-10 text-sm text-white rounded-md uppercase tracking-widest bg-gradient-to-r from-gradient-1 to-gradient-2'>Learn More</button>
+            <button className='w-full py-3 px-4 mt-10 text-sm text-white rounded-md uppercase tracking-widest bg-gradient-to-r from-gradient-1 to-gradient-2 hover:bg-none hover:text-gradient-2 border border-transparent hover:border-gradient-2 transition-colors ease-in duration-150'>Learn More</button>
           </div>
         </div>
       </div>
